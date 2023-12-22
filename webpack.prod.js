29663e8f64700f0d9a2e0aed9782c4e222ea52ec
Arman <armangulistani@gmail.com>
2023-12-22 13:25:37 -0500

    Reordered imported packages
    Reordered the imported packages insdie of webpack.common, webpack.dev and webpack.prod to stop eslint from giving me errors.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const common = require("./webpack.common");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "production",
